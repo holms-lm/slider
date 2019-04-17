1049d42f6043f097ba56b480062b4abcc7a17a62
holms <serg882244@gmail.com>
2019-04-18 00:49:40 +0300

    slider on click

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-console.log('1234');
+$(function () {
+    var sliderWidth = '1500px';
+    var currentN = 0;
+    function start(){
+        $('#slider').css('width', sliderWidth);
+        console.log('start');
+    }
+
+    function slider() {
+
+    }
+    function next(n) {
+        console.log(n);
+        var leftSlide = -n*300 + 'px'
+        $('#slider').css('left', leftSlide);
+    }
+    $('#slider').click(function () {
+        currentN = currentN + 1;
+        if (currentN>4){currentN=0;}
+        next(currentN);
+    });
+    start();
+
+});
