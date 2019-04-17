5eb839527f737c170eb688f9bb295df6415184d7
holms <serg882244@gmail.com>
2019-04-18 01:29:02 +0300

    slider auto slide with hover pause

--- src/main.js
+++ src/main.js
@@ -1,24 +1,46 @@
 $(function () {
-    var sliderWidth = '1500px';
+    var slideWidth = $('#slider').width();
+    // var contLi = $('.slider__item').length;
+    var quantityLi = $('#slider').children().length;
+    var sliderWidth = slideWidth * quantityLi + 'px';
     var currentN = 0;
+    var leftSlide;
+    start();
     function start(){
         $('#slider').css('width', sliderWidth);
-        console.log('start');
+        console.log('start. count = ' + quantityLi);
     }
 
-    function slider() {
-
-    }
     function next(n) {
-        console.log(n);
-        var leftSlide = -n*300 + 'px'
+        leftSlide = -n * slideWidth + 'px'
         $('#slider').css('left', leftSlide);
     }
+
+    function countSlider() {
+        currentN += 1;
+        if (currentN>4){currentN=0;}
+        next(currentN);
+
+    }
+    var timerId = setInterval(function() {
+        countSlider();
+    }, 2000);
+
+    $('#slider').hover(
+        function () {
+            clearInterval(timerId);
+        },
+        function () {
+            timerId = setInterval(function() {
+                countSlider();
+            }, 2000);
+        }
+    )
+
     $('#slider').click(function () {
         currentN = currentN + 1;
         if (currentN>4){currentN=0;}
         next(currentN);
     });
-    start();
 
 });
